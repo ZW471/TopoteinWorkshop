42013904871b2888b2804ef958913f53e4de9952
Zhiyu Wang <zw471@cam.ac.uk>
2025-02-17 17:46:19 +0000

    safer random protein

--- proteinworkshop/utils/test.py
+++ proteinworkshop/utils/test.py
@@ -2,9 +2,21 @@ import random
 
 from graphein.protein.tensor import Protein
 
+PDB_LIST = ["3eiy", "1hcn", "9dlx", "8zdd"]
+PDB_LIST_SAFE = []
 
-def get_random_protein_safe() -> "Protein":
+
+def get_random_protein_safe(pdb=None) -> "Protein":
     """Utility/testing function to get a random proteins."""
-    pdbs = ["3eiy", "4hhb", "1hcn"]
-    pdb = random.choice(pdbs)
+    if pdb is None:
+        pdbs = PDB_LIST_SAFE
+        pdb = random.choice(pdbs)
     return Protein().from_pdb_code(pdb)
+
+
+for pdb in PDB_LIST:
+    try:
+        get_random_protein_safe(pdb=pdb)
+        PDB_LIST_SAFE.append(pdb)
+    except ValueError:
+        print(f"Error with pdb code {pdb} in random protein function, removed.")

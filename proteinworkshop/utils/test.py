78621d91dc550e6a5fcc5b29ac4fffb37f21cc70
Zhiyu Wang <zcabanq@ucl.ac.uk>
2025-02-17 21:25:02 +0000

    fixing random protein from pdb error - use local file to deal with network error, etc

--- proteinworkshop/utils/test.py
+++ proteinworkshop/utils/test.py
@@ -1,22 +1,49 @@
 import random
-
+import torch
 from graphein.protein.tensor import Protein
+import proteinworkshop.constants
+import os
+import warnings
 
-PDB_LIST = ["3eiy", "1hcn", "9dlx", "8zdd"]
-PDB_LIST_SAFE = []
+PDB_LIST = ["3eiy", "1hcn", "8py0", "8vth"]
 
 
-def get_random_protein_safe(pdb=None) -> "Protein":
+def get_random_protein_safe(pdb=None, offline=True) -> "Protein":
     """Utility/testing function to get a random proteins."""
     if pdb is None:
-        pdbs = PDB_LIST_SAFE
-        pdb = random.choice(pdbs)
-    return Protein().from_pdb_code(pdb)
+        pdb = random.choice(PDB_LIST)
+
+    if offline:
+        try:
+            return torch.load(os.path.join(proteinworkshop.constants.DATA_PATH, "sample_proteins", f"{pdb}.pt"), weights_only=False)
+        except FileNotFoundError:
+            return get_random_protein_safe(pdb=pdb, offline=False)
+    else:
+
+        print("downloading pdb files from internet - this can cause issues sometimes!")
+        protein = Protein().from_pdb_code(pdb)
+
+        data_dir = proteinworkshop.constants.DATA_PATH
+        print(f'Saving protein {pdb} to {data_dir}...')
+        torch.save(protein, os.path.join(data_dir, "sample_proteins", f"{pdb}.pt"))
+
+        return protein
 
 
-for pdb in PDB_LIST:
-    try:
-        get_random_protein_safe(pdb=pdb)
-        PDB_LIST_SAFE.append(pdb)
-    except ValueError:
-        print(f"Error with pdb code {pdb} in random protein function, removed.")
+
+if __name__ == "__main__":
+
+    data_dir = proteinworkshop.constants.DATA_PATH
+    print(data_dir)
+
+    for pdb in PDB_LIST:
+        print(pdb)
+        try:
+            protein = get_random_protein_safe(pdb=pdb, offline=False)
+            print(protein)
+        except Exception:
+            print(f"Error with pdb code {pdb} in random protein function, removed.")
+            continue
+
+        print('saving...')
+        torch.save(protein, os.path.join(data_dir, "sample_proteins", f"{pdb}.pt"))
